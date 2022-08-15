2f0a4da626550b4b60e6c4dd7cf697c0d3d0d41c
Waamer <waamer1a@gmail.com>
2022-08-15 18:44:56 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function chbg(color) {
+    const Icon = document.getElementsByClassName("ti-home-2");
+
+    Icon[0].style.color = color;
+} 
+function chbg2(color) {
+    const Icon = document.getElementsByClassName("ti-info-circle");
+
+    Icon[0].style.color = color;
+} 
